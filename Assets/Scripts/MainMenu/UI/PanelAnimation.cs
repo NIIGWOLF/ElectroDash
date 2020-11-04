72a358c3b9922496ac378980b622fc387d9ab631
Elena Protasova <lll333ll@mail.ru>
2020-11-04 13:53:41 +0300

    Добавила анимацию в меню

---
+++ Assets/Scripts/MainMenu/UI/PanelAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class PanelAnimation : MonoBehaviour
+{
+    public Vector2 targetPosition;
+    public Vector2 startPosition;
+    float speed = 0.5f;
+    public void GoToTarget(){
+        gameObject.GetComponent<RectTransform>().DOAnchorPos(targetPosition, speed);
+    }
+    public void GoBack(){
+        gameObject.GetComponent<RectTransform>().DOAnchorPos(startPosition, speed);
+    }
+
+}
