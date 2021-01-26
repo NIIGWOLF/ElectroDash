100be5ecb1b7a86d8db22b16fbe7d5c69f5e9c6d
Dmitry Klyushkin <nigwolf10@gmail.com>
2021-01-26 12:52:17 +0300

    убрал появление партиклов при старте сцены

--- Assets/Scripts/StartDeathPS.cs
+++ Assets/Scripts/StartDeathPS.cs
@@ -5,10 +5,11 @@ public class StartDeathPS : MonoBehaviour
     public GameObject PSCreate;
     public int orderLayer=0;
     bool exit = false;
-    void Start()
+    void OnEnable()
     {
         Sprite sp = gameObject.GetComponent<SpriteRenderer>().sprite;
         GameObject gops=Instantiate(PSCreate,transform.position,Quaternion.Euler(transform.eulerAngles));
+        gops.GetComponent<ParentPS>().parent=gameObject;
         var ps = gops.GetComponent<ParticleSystem>();
         ps.GetComponent<ParticleSystemRenderer>().sortingOrder=orderLayer;
         var shps = ps.shape;
@@ -19,6 +20,7 @@ public class StartDeathPS : MonoBehaviour
         if (!exit){
         Sprite sp = gameObject.GetComponent<SpriteRenderer>().sprite;
         GameObject gops=Instantiate(PSCreate,transform.position,Quaternion.Euler(transform.eulerAngles));
+        gops.GetComponent<ParentPS>().parent=gameObject;
         var ps = gops.GetComponent<ParticleSystem>();
         ps.GetComponent<ParticleSystemRenderer>().sortingOrder=orderLayer;
         var shps = ps.shape;
