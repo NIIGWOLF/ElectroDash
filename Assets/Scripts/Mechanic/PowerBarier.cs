3497d4bac76ff1a98bb98e0ba69abbd873f7734a
Dmitry Klyushkin <nigwolf10@gmail.com>
2021-02-02 19:38:37 +0300

    Доделал генератор силового поля

---
+++ Assets/Scripts/Mechanic/PowerBarier.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerBarier : MonoBehaviour
+{
+    protected virtual void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.GetComponent<Character>())
+        {
+            
+            Character go = collider.gameObject.GetComponent<Character>();
+            go.returnBack();
+        }
+    }
+}
