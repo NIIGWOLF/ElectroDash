0c084ff8a3299288c98b388ae7e53aa9e31b12f0
Dmitry Klyushkin <nigwolf10@gmail.com>
2021-02-02 20:11:39 +0300

    Добавил партиклы стекла

---
+++ Assets/Scripts/System/TimeDestroyPS.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeDestroyPS : MonoBehaviour
+{
+    [Range(0f,10f)] public float time = 1;
+    void Start()
+    {
+        Invoke("DestroyPS",time);
+    }
+
+    void DestroyPS(){
+        Destroy(gameObject);
+    }
+
+}
