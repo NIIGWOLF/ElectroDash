7bc8aafddde22ab25406ae63a3be085f7047f72c
lXabba <lll333ll@mail.ru>
2021-02-05 11:16:21 +0300

    переделала звезды смерти

--- Assets/Scripts/LevelUI/StartPause.cs
+++ Assets/Scripts/LevelUI/StartPause.cs
@@ -6,12 +6,14 @@ public class StartPause : MonoBehaviour
 {
     
     public void pause(){
+        Invoke("method",0.5f);
+    }
+    public void method(){
         if (Time.timeScale != 0 ) {
             Time.timeScale = 0;
            }
         else {
             Time.timeScale = 1;
         }
-
     }
 }
