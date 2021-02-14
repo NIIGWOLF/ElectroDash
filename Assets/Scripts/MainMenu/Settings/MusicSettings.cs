2b86330937d102c6f3f31987f4205f104a65dc85
lXabba <lll333ll@mail.ru>
2021-02-14 20:36:32 +0300

    музыка в настройках

---
+++ Assets/Scripts/MainMenu/Settings/MusicSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MusicSettings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+          gameObject.GetComponent<Slider>().value = MainMenuManager.audioData.audio.musicVolume;
+        
+    }
+
+    // Update is called once per frame
+   public void soundsChange(){
+        foreach(var sound in MainMenuManager.uiMainMenuManager.music.GetComponentsInChildren<AudioSource>()){
+            sound.volume = gameObject.GetComponent<Slider>().value;
+        }
+        MainMenuManager.audioData.audio.musicVolume = gameObject.GetComponent<Slider>().value;
+        MainMenuManager.audioData.SaveData();
+    }
+}
