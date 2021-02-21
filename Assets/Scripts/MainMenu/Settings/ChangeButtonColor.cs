0e83b15d3ea6a5ffa0ab607a4a8cb5dffba29d51
lXabba <lll333ll@mail.ru>
2021-02-21 14:43:32 +0300

    какие-то изменения

---
+++ Assets/Scripts/MainMenu/Settings/ChangeButtonColor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ChangeButtonColor : MonoBehaviour
+{
+    public Color selectedColor;
+    public Color normalColor;
+    public void ChangeColor(){
+        gameObject.GetComponent<Image>().color = selectedColor;
+    }
+    public void NormalColor(){
+        gameObject.GetComponent<Image>().color = normalColor;
+    }
+}
