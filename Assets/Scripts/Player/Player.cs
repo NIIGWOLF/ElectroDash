b3f827d8cbb73d75a6d021d88ffc9fdc65c51645
Dmitry Klyushkin <nigwolf10@gmail.com>
2021-02-22 10:45:21 +0300

    Update Player.cs

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -115,10 +115,8 @@ public class Player : Character
     public override void Die()
     {
         Handheld.Vibrate();
-        ScriptManager.objectManager.AllCharacter.Remove(gameObject);
-        var go = Instantiate(deathPlayer,transform.position,Quaternion.identity);
         Instantiate(deathPS,transform.position,Quaternion.identity);
-        go.AddComponent<RestartAfterDeath>();
-        Destroy(gameObject);
+        gameObject.AddComponent<RestartAfterDeath>();
+        Destroy(gameObject.GetComponent<Player>());
     }
 }
