848a7c1d095c9e58665c9c6af66818f5396b32fa
Dmitry Klyushkin <nigwolf10@gmail.com>
2021-03-07 14:48:21 +0300

    ActivBlockPoint

---
+++ Assets/Scripts/Point/ActivBlockPoint.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivBlockPoint : ActivPoint
+{
+    //public SpriteRenderer button;
+    public Sprite Green;
+    public Sprite Grey;
+
+    void Awake(){
+        //button.color = new Color(1,1,1,1f);
+    }
+
+    public override Vector3Int InComming(Vector3Int backPos, bool activPoint, GameObject character)
+    {
+        if (activ)
+        {
+            return NextPos(backPos);
+        }
+        else{
+            return backPos;
+        }
+        
+    }
+
+    public override void SetActiv(bool activ){
+        if (this.activ==activ) return;
+        this.activ=activ;
+        gameObject.GetComponent<ActivElement>().WireActivAll(activ);
+        if (activ) {
+            //button.color = new Color(1,0,0,1);
+            gameObject.GetComponent<SpriteRenderer>().sprite=Green;
+        }
+        else {
+            //button.color = new Color(1,1,1,1f);
+            gameObject.GetComponent<SpriteRenderer>().sprite=Grey;
+        }
+    }
+}
