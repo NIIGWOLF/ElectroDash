e4aaf50f9df56eb49b5f9e342dbb28b24e5868bf
Dmitry Klyushkin <nigwolf10@gmail.com>
2021-04-19 09:58:27 +0300

    монетизация

---
+++ Assets/Scripts/System/Ad.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Ad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Advertisement.isSupported){
+            Advertisement.Initialize("4092201",true);
+        }
+        Invoke("pokaz",2);
+    }
+
+    void pokaz(){
+        print("pokaz");
+        if (Advertisement.IsReady()){
+            print("Show");
+            Advertisement.Show();
+        }
+        else
+        print("Don't show");
+        
+    }
+}
