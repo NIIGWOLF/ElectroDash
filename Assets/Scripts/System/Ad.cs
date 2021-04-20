723017376216b2c7353d1199232616beabcf4f1e
Dmitry Klyushkin <nigwolf10@gmail.com>
2021-04-20 13:37:09 +0300

    добавлена реклама для подсказок

--- Assets/Scripts/System/Ad.cs
+++ Assets/Scripts/System/Ad.cs
@@ -1,46 +1,29 @@
 ﻿using UnityEngine;
-using UnityEngine.Advertisements;
 
-public class Ad : MonoBehaviour, IUnityAdsListener
+public class Ad : MonoBehaviour
 {
-    public void OnUnityAdsDidError(string message)
-    {
+    public void ShowAddMap(){
+        AdEvent.Instance.ShowAddMap(this);
     }
 
-    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
-    {
-        if (showResult == ShowResult.Finished){
-            print("Реклама досмотрена до конца");
-            //реклама просмотрена до конца
-        }else if(showResult == ShowResult.Failed){
-            print("Реклама пропущена");
-            //что-то пошло не так
-        }
+    public void ShowAddHelp(){
+        AdEvent.Instance.ShowAddHelp(this);
     }
 
-    public void OnUnityAdsDidStart(string placementId)
-    {
+    public void ShowAddCoin(){
+        AdEvent.Instance.ShowAddCoin(this);
     }
 
-    public void OnUnityAdsReady(string placementId)
-    {
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Advertisement.AddListener(this);
-        if (Advertisement.isSupported){
-            Advertisement.Initialize("4092201",true);
-        }
-    }
-
-    public void ShowAd(){
-        if (Advertisement.IsReady("Rewarded_Android")){
-            Advertisement.Show("Rewarded_Android");
-            print("Реклама запущена");
-        }else
-            print("Реклама не готова");
-        
+    public void AddValue(AdEvent.Add add){
+        switch(add){
+                case AdEvent.Add.Map:
+                    GetComponent<BuyHint>().BuyMapHint();
+                break;
+                case AdEvent.Add.Help:
+                    GetComponent<BuyHint>().BuySimpleHint();
+                break;
+                case AdEvent.Add.Coin:
+                break;
+            }
     }
 }
---
+++ Assets/Scripts/System/AdEvent.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine.Advertisements;
+using UnityEngine;
+
+public class AdEvent : Singleton<AdEvent>, IUnityAdsListener 
+{
+    public enum Add{ Map, Help, Coin};
+    private Add add;
+    private Ad ad;
+    
+    public void OnUnityAdsDidError(string message)
+    {
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        if (showResult == ShowResult.Finished){
+            print("Реклама досмотрена до конца");
+            switch(add){
+                case Add.Map:
+                    ad.AddValue(Add.Map);
+                break;
+                case Add.Help:
+                    ad.AddValue(Add.Help);
+                break;
+                case Add.Coin:
+                    ad.AddValue(Add.Coin);
+                break;
+            }
+            //реклама просмотрена до конца
+        }else if(showResult == ShowResult.Failed){
+            print("Реклама пропущена");
+            //что-то пошло не так
+        }
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Advertisement.AddListener(this);
+        if (Advertisement.isSupported){
+            Advertisement.Initialize("4092201",true);
+        }
+    }
+
+    public void ShowAddMap(Ad ad){
+        this.ad = ad;
+        if (Advertisement.IsReady("Rewarded_Android")){
+            Advertisement.Show("Rewarded_Android");
+            add=Add.Map;
+            print("Реклама запущена");
+        }else
+            print("Реклама не готова");
+    }
+
+    public void ShowAddHelp(Ad ad){
+        this.ad = ad;
+        if (Advertisement.IsReady("Rewarded_Android")){
+            Advertisement.Show("Rewarded_Android");
+            add=Add.Help;
+            print("Реклама запущена");
+        }else
+            print("Реклама не готова");
+    }
+
+    public void ShowAddCoin(Ad ad){
+        this.ad = ad;
+        if (Advertisement.IsReady("Rewarded_Android")){
+            Advertisement.Show("Rewarded_Android");
+            add=Add.Coin;
+            print("Реклама запущена");
+        }else
+            print("Реклама не готова");
+    }
+}
