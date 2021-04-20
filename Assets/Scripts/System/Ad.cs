54d0425a69467e308099e5d2609cc404badbe8f1
Dmitry Klyushkin <nigwolf10@gmail.com>
2021-04-20 12:35:41 +0300

    изменение частиц при появлении

--- Assets/Scripts/System/Ad.cs
+++ Assets/Scripts/System/Ad.cs
@@ -1,27 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Advertisements;
 
-public class Ad : MonoBehaviour
+public class Ad : MonoBehaviour, IUnityAdsListener
 {
+    public void OnUnityAdsDidError(string message)
+    {
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        if (showResult == ShowResult.Finished){
+            print("Реклама досмотрена до конца");
+            //реклама просмотрена до конца
+        }else if(showResult == ShowResult.Failed){
+            print("Реклама пропущена");
+            //что-то пошло не так
+        }
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        Advertisement.AddListener(this);
         if (Advertisement.isSupported){
             Advertisement.Initialize("4092201",true);
         }
-        Invoke("pokaz",2);
     }
 
-    void pokaz(){
-        print("pokaz");
-        if (Advertisement.IsReady()){
-            print("Show");
-            Advertisement.Show();
-        }
-        else
-        print("Don't show");
+    public void ShowAd(){
+        if (Advertisement.IsReady("Rewarded_Android")){
+            Advertisement.Show("Rewarded_Android");
+            print("Реклама запущена");
+        }else
+            print("Реклама не готова");
         
     }
 }
