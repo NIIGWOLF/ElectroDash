6b0005bf5d41dc681fe1249ce8c275bffb47e669
lXabba <lll333ll@mail.ru>
2021-04-20 15:38:42 +0300

    7 levels lena

--- Assets/Scripts/LevelUI/UseMap.cs
+++ Assets/Scripts/LevelUI/UseMap.cs
@@ -4,12 +4,13 @@ using UnityEngine.UI;
 public class UseMap : MonoBehaviour
 {
     bool isUsedMap = false;
+    public float valueZoom = 6f;
     //public GameObject mapHint;
     public void useMap()
     {
         if (HintData.Instance.hint.mapHint > 0 && !isUsedMap)
         {
-            ScriptManager.objectManager.moveSizeCamera.zoomOut();
+            ScriptManager.objectManager.moveSizeCamera.zoomOut(valueZoom);
 
             HintData.Instance.hint.mapHint--;
             HintData.Instance.SaveData();
