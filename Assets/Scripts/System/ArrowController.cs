f8e2e1d1285d61197cfbded58a4a2ca9511e9b6c
Dmitry Klyushkin <nigwolf10@gmail.com>
2021-04-21 22:35:43 +0300

    ui стрелки

---
+++ Assets/Scripts/System/ArrowController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+    Player player;
+
+    void Start(){
+        Invoke("Init",1);
+    }
+    void Init(){
+        player = ScriptManager.player.GetComponent<Player>();
+    }
+    public void downKey(int dir){
+        player.Swipe(dir);
+    }
+}
